bf8412f933e2ffa229d99e878292ecb0d7cd4a32
Bandervelik <mikael.olizar@gmail.com>
2023-11-19 21:17:52 +0200

    2уу

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const toggle = document.getElementById('toggleDark');
-const body = document.querySelector('body');
+// Check if the user has a preferred color scheme
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-toggle.addEventListener('click', function(){
-    this.classList.toggle('bi-moon');
-    if(this.classList.toggle('bi-brightness-high-fill')){
-        body.style.background = 'white';
-        body.style.color = 'black';
-        body.style.transition = '2s';
-    }else{
-        body.style.background = 'black';
-        body.style.color = 'white';
-        body.style.transition = '2s';
-    }
-});
+// Set the initial theme based on user preference or default to light
+const currentTheme = localStorage.getItem("theme") || (prefersDarkScheme.matches ? "dark" : "light");
+document.body.classList.toggle("dark-mode", currentTheme === "dark");
+
+// Toggle between light and dark themes
+function toggleTheme() {
+  const isDarkMode = document.body.classList.toggle("dark-mode");
+  const theme = isDarkMode ? "dark" : "light";
+  localStorage.setItem("theme", theme);
+}
+
+// Attach the toggleTheme function to a button or any other trigger
+// Example: <button onclick="toggleTheme()">Toggle Theme</button>
